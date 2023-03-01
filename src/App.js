4279434ff969d2f39a00940a8b6ae40356e54fe7
mohammad-a-hossain <adam.eve007@gmail.com>
2023-03-01 21:59:09 +0600

    scaffolding a new project name time-zone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+//import './App.css';
+
+function App() {
+  return (
+    <div className="sm:bg-cyan-300 lg:bg-slate-900 h-screen ">
+      <h1>hello </h1>
+      <h2 className="text-green-700"> this is another </h2>
+    </div>
+  );
+}
+
+export default App;
